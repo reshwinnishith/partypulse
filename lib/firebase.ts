556393ae8256c9df70033ebecb9517696d53cbe0
reshwinnishith <reshwinnishith@gmail.com>
2026-05-28 15:56:00 +0530

    fix: remove unused imports

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp, getApps, getApp } from 'firebase/app'
-import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from 'firebase/firestore'
+import { initializeFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
